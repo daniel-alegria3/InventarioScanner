871ae3ab114bf2079a651a16ca6848963ed3bc3e
Daniel Alegria Sallo <ccusdanielalegriasallo@gmail.com>
2025-10-26 10:22:01 -0500

    Updates

--- assets/ios.transition-legacy-Cpl2ke6s.js
+++ assets/ios.transition-legacy-Cpl2ke6s.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BeSyfchU.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.U,n=t.V}],execute:function(){
+System.register(["./index-legacy-CL1tyAua.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.U,n=t.V}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
